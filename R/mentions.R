d44925810aa7fdf0e5ad17b01a27d9ffab72a06d
Andrew Heiss <andrewheiss@gmail.com>
2014-08-01 16:40:46 -0400

    Add planned TODOs

--- R/mentions.R
+++ R/mentions.R
@@ -18,7 +18,7 @@ orgs.mentioned <- all.mentions %>%
   summarize(mentions=sum(num.mentions)) %>%
   arrange(desc(mentions)) %>%
   filter(mentions > 10)
-# TODO: Look through list and see why there are so many 0s, like Hisham Mubarak
+# BUG: Look through list and see why there are so many 0s, like Hisham Mubarak
 
 # Disaggregate top organizations by publication
 mention.summary <- all.mentions %>%
@@ -38,3 +38,10 @@ p + geom_bar(stat="identity", position="dodge") + coord_flip() +
   theme(legend.position="bottom", legend.key.size=unit(.7, "line"), 
         legend.key=element_blank()) + 
   scale_fill_manual(values=c("#e41a1c", "#377eb8", "#e6ab02"), name="") 
+
+# TODO: Topics by organization by publication? Average topics for articles where NGO is mentioned
+# TODO: Top organizations for each topic?
+# TODO: How are organizations used? Press release vs. website vs. quoted person (who - kind of person, spokesperson, activist, etc.) vs. just a mention (+some sort of international tie?)
+# TODO: Pull sentences of mention ±1 sentence for context
+# TODO: Figure out sentence topics?
+# TODO: Automate way of determining source usage? 
