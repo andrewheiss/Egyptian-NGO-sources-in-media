9edd81eabe0d150f23830abddbcb03ce0ec5e281
Andrew Heiss <andrewheiss@gmail.com>
2014-08-01 15:59:54 -0400

    Basic plot of mentions by organization by publication

--- R/mentions.R
+++ R/mentions.R
@@ -1,11 +1,40 @@
-all.mentions %>%
-  group_by(publication, organization) %>%
-  summarize(thing=sum(num.mentions))
+# Title:          mentions.R
+# Description:    Analyze NGO mentions
+# Author:         Andrew Heiss
+# Last updated:   2014-08-01
+# R version:      ≥3.0
 
-all.mentions %>%
+# Load packages
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(ggplot2))
+library(grid)
+
+# Load data
+load("../Output/media_data.RData")
+
+# Find which organizations are cited the most
+orgs.mentioned <- all.mentions %>%
   group_by(organization) %>%
-  summarize(thing=sum(num.mentions))
+  summarize(mentions=sum(num.mentions)) %>%
+  arrange(desc(mentions)) %>%
+  filter(mentions > 10)
+# TODO: Look through list and see why there are so many 0s, like Hisham Mubarak
 
-all.mentions %>%
-  group_by(publication) %>%
-  summarize(thing=sum(num.mentions))
+# Disaggregate top organizations by publication
+mention.summary <- all.mentions %>%
+  group_by(publication, organization) %>%
+  summarize(mentions=sum(num.mentions)) %>%
+  arrange(desc(mentions)) %>%
+  ungroup() %>%
+  filter(organization %in% orgs.mentioned$organization) %>%
+  mutate(organization = ordered(organization, levels=rev(unique(organization)))) %>%
+  mutate(publication = factor(publication, labels=c("Al-Ahram  ", "Daily News Egypt  ", "Egypt Independent")))
+
+# Make a pretty graph
+p <- ggplot(mention.summary, aes(x=organization, y=mentions, fill=publication))
+p + geom_bar(stat="identity", position="dodge") + coord_flip() + 
+  labs(x=NULL, y="Articles where mentioned") + 
+  theme_bw(10) + 
+  theme(legend.position="bottom", legend.key.size=unit(.7, "line"), 
+        legend.key=element_blank()) + 
+  scale_fill_manual(values=c("#e41a1c", "#377eb8", "#e6ab02"), name="") 
