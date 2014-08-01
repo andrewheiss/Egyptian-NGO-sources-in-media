51a56e6e39686a5f6130d7810173ed2bc8dee32a
Andrew Heiss <andrewheiss@gmail.com>
2014-08-01 12:42:38 -0400

    Initial commit

---
+++ Python/process_natural_language.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import re
+import string
+import nltk
+from nltk.collocations import *
+import glob
+import codecs
+import os
+from itertools import chain
+import csv
+import argparse
+
+
+# Get command line information
+parser = argparse.ArgumentParser(description='Stem and create bigrams for a folder of plain text files.')
+parser.add_argument('documents', type=str, 
+                    help='the path to the folder of exported documents')
+parser.add_argument('output_folder', type=str, 
+                    help='the path to save final stemmed text files')
+parser.add_argument('stopwords', type=argparse.FileType('r'), 
+                    help='a list of stopwords to remove')
+parser.add_argument('bigram_csv', type=argparse.FileType('wb'), 
+                    help='CSV of most common bigrams')
+args = parser.parse_args()
+
+
+#------------
+# Variables
+#------------
+# Save arguments
+path_to_documents = os.path.abspath(args.documents) + "/*"  # Must have * for glob
+output_folder = os.path.abspath(args.output_folder)
+stopword_file = args.stopwords
+csv_path = args.bigram_csv
+
+# Load MALLET stopwords
+stopwords = set([word.strip() for word in stopword_file])  # Using set() speeds up "not in" searches
+
+# Select the stemming algorithm
+stemmer = nltk.stem.snowball.EnglishStemmer()  # Newest, made by Porter in 2001(?)
+# stemmer = nltk.stem.porter.PorterStemmer()  # From 1980
+# stemmer = nltk.stem.lancaster.LancasterStemmer()  # From 1990
+
+# Minimum n-gram frequency
+bigram_min = 10
+trigram_min = 10
+
+
+#-------------------
+# Helper functions
+#-------------------
+# Strip punctuation
+def remove_punc(text):
+  punc = string.punctuation.replace('-', '') + u'–—”’“‘ '  # Define punctuation
+  regex = re.compile('[%s]' % re.escape(punc))
+  content_no_punc = regex.sub(' ', text.lower())  # Remove punctuation and make
+  return(content_no_punc)
+
+
+# Loop through all the words in the document, find adjacent unigrams that
+# match significat bigrams, and replace them with an underscore-separated
+# token. For example, given these bigrams: 
+#
+#   bigrams = [('apple', 'orange'), ('happy', 'day'), ('big', 'house')]
+#
+# it will transform this list of words:
+#
+#   words = ['apple', 'orange', 'boat', 'car', 'happy', 'day', 'cow']
+#
+# into this:
+#
+#   words_fixed = replace_bigrams(words, bigrams)
+#               = ['apple_orange', 'boat', 'car', 'happy_day', 'cow']
+#
+# This is slower than other functions that could use a dict or set (see
+# http://stackoverflow.com/questions/22366659/match-adjacent-list-elements-with-a-list-of-tuples-in-python/),
+# but it works correctly. Other methods with itertools, etc. fail because 
+# they assume unique words in each bigram and create a dict with the first 
+# word in the pair as the key.
+#
+# Actually, the biggest bottleneck is the collection of bigrams.
+# Using `set([pair[0] for pair in bigrams_significant])` runs in 15ish seconds, 
+# while `[pair[0] for pair in bigrams_significant]` takes 60ish seconds. Yikes.
+# Using a set is significantly faster, but it discards the ordering, which is bad.
+
+def replace_bigrams(words, bigrams):
+  words_fixed = []
+  last = None
+  for word in words:
+    if (last, word) in bigrams:
+      words_fixed.append("{0}_{1}".format(last, word))
+      last = None
+    else:
+      if last:
+        words_fixed.append(last)
+      last = word
+  if last:
+    words_fixed.append(last)
+  return(words_fixed)
+
+
+#---------------
+# Process text
+#---------------
+# Load documents
+documents = glob.glob(path_to_documents)
+
+# Initialize corpus-wide vocabulary
+vocabulary = {}
+
+for text_file in documents:
+  # Must use codecs.open() because Unicode in Python 2.x sucks. 
+  document = codecs.open(text_file, 'r', 'utf-8').read()
+
+  # Remove punctuation, clean up whitespace, and convert to a list
+  words = remove_punc(document).strip().split()
+
+  # Remove stopwords
+  # al- and el- aren't taken care of in stopwords, so they have to manually be removed
+  no_stopwords = [word.replace('el-', '').replace('al-', '') for word in words if word not in stopwords]
+
+  # Stem remaining words
+  stemmed = [stemmer.stem(word) for word in no_stopwords]
+
+  # Add tokens to corpus vocabulary (with file name as key)
+  vocabulary[os.path.basename(text_file)] = stemmed
+
+# Create new flat corpus vocabulary
+token_list = list(chain.from_iterable(vocabulary.values()))
+
+
+#------------------------------------------
+# Find most important bigram collocations
+#------------------------------------------
+# See https://nltk.googlecode.com/svn/trunk/doc/howto/collocations.html
+
+# Bigrams
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+bigram_finder = BigramCollocationFinder.from_words(token_list)
+bigram_finder.apply_freq_filter(bigram_min)
+
+# Get top bigrams
+# Other ways of scoring bigrams: https://github.com/AJRenold/classification_assignment_i256
+# Tecnically .nbest() is easier, but it doesn't return the actual association score
+# ngram_limit = int(len(token_list) * 0.1)
+# bigrams_pmi = bigram_finder.nbest(bigram_measures.pmi, ngram_limit)
+
+# So use .score_ngrams() and subset the list manually
+bigrams_likerat = bigram_finder.score_ngrams(bigram_measures.likelihood_ratio)
+
+# Select only super significant bigrams
+# Instead of making people install scipy, it's probably easiest to just use R for the stats stuff
+# scipy.stats.chi2.ppf(0.999, 1) = qchisq(0.999, df=1) = 10.82757
+critical_value = 10.82757
+bigrams_significant = [bigram for bigram in bigrams_likerat if bigram[1] > critical_value]
+
+bigrams_out = [[bigram[1], bigram[0][0], bigram[0][1]] for bigram in bigrams_significant]
+
+with csv_path as csv_file:
+  csv_out = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+  csv_out.writerow(['-2LL', 'W1', 'W2'])
+  for row in bigrams_out:
+    csv_out.writerow(row)
+
+
+# MAYBE: Make a table like p. 163 in Manning and Schutze for top x bigrams?
+# MAYBE: Plot likelihood ratio for bigrams, just for fun?
+
+# Trigrams
+# Necessary? Tricky because stuff like "human right" is a significant bigram,
+# but also present in lots of trigrams: "violat human right", "human right
+# group", "human right lawyer", etc.
+# trigram_measures = nltk.collocations.TrigramAssocMeasures()
+# trigram_finder = TrigramCollocationFinder.from_words(token_list)
+# trigram_finder.apply_freq_filter(trigram_min)
+
+# trigrams_likerat = trigram_finder.score_ngrams(trigram_measures.likelihood_ratio)
+# print(trigrams_likerat[:10])
+
+
+#--------------------------------
+# Create clean, final documents
+#--------------------------------
+# Extract just the bigram tuples from the ngram score nested list
+bigrams = [pair[0] for pair in bigrams_significant]
+
+# Loop through all documents in the vocabulary, join/replace bigrams, and save to disk
+for document in vocabulary:
+  words = vocabulary[document]
+  words_fixed = replace_bigrams(words, bigrams)
+
+  filename = output_folder + '/' + document
+  with open(filename, 'w') as f:
+    f.write(" ".join(words_fixed).encode('utf8'))  # Die, Unicode in Python 2.x, DIE!
---
+++ R/create_topic_model.R
@@ -0,0 +1,214 @@
+# Title:          create_topic_model.R
+# Description:    Build topic model with MALLET
+# Author:         Andrew Heiss
+# Last updated:   2014-07-31
+# R version:      ≥3.0
+
+# Load libraries and set initial working directory
+suppressPackageStartupMessages(library(reshape2))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(plyr))
+suppressPackageStartupMessages(library(pander))
+
+# Process command line arguments
+source("get_args.R")  # Better handling of arguments
+args <- getArgs(defaults=list(control=FALSE))
+control = args$control
+
+# Set seed for MALLET
+mallet.seed <- 1234
+
+# Make sure MALLET is there
+if(!file.exists("../mallet/bin/mallet")) {
+  stop("MALLET not found. You need to place MALLET in a folder named 'mallet' in the root of this project.")
+}
+
+
+#------------------------
+# Create MALLET command
+#------------------------
+if(control == FALSE) {
+  # Folder with input text files
+  # TODO: Make this work with spaces. shQuote(file.path(importdir)) should work, but the quoted path breaks MALLET
+  import.dir <- "articles_stemmed"
+
+  # Set names for output files 
+  output.file <- "topics.mallet"  # MALLET data file
+  output.state <-  "topic-state.gz"  # List of every word in every article and which topics they're assigned to
+  output.topickeys <- "topic-keys.txt"  # List of the topics
+  output.doctopics <- "topic-doctopics.txt"  # Proportion of each topic in each input file
+
+  output.rdata <- "../Output/topic_model.RData"
+  output.csv <- "../Output/topic-docs.csv"
+
+  # SPSA short names
+  # short.names <- c("National government", "Draft constitution", "Environmental issues",
+  #                  "Development", "Police arrests", "Protests and clashes", 
+  #                  "Sexual violence", "Police torture", "Elections", "Human rights and civil society", 
+  #                  "Post-revolutionary Egypt", "Business and government", "Egyptian workers", 
+  #                  "Trials", "Religious issues", "Legislation", "Morsi and press freedom",
+  #                  "SCAF", "Youth in the street", "Christian issues")
+
+  # Shortnames for enhanced corpus (stemmed, n-grammed)
+  short.names <- c("Police torture", "Sexual violence", "Media and censorship", 
+                   "Sectarian issues", "Egyptian workers", "Religious issues", 
+                   "Police violence", "Business", "Protests and clashes", 
+                   "Muslim Brotherhood and constitution", "Elections", "Military trials", 
+                   "Legislation and governance", "Environmental issues", 
+                   "Human rights and civil society", "Protestors and activism", 
+                   "Public economics", "Police arrests", "Muslim Brotherhood and politics", 
+                   "Post-revolutionary Egypt (catch-all)")
+} else {
+  # "Control" group of articles
+  import.dir <- "articles_control_stemmed"
+
+  # Set names for output files 
+  output.file <- "topics_control.mallet"
+  output.state <-  "topic_control-state.gz"
+  output.topickeys <- "topic_control-keys.txt"
+  output.doctopics <- "topic_control-doctopics.txt"
+
+  output.rdata <- "../Output/topic_model_control.RData"
+  output.csv <- "../Output/topic-docs_control.csv"
+
+  # Control group short names
+  short.names <- c("Muslim Brotherhood and politics", "Morsi and the media", 
+                   "Miscellaneous", "Unions and strikes", "Trials", "Egypt (catch-all)", 
+                   "Religious issues", "Syrian civil war", "Cairo affairs", 
+                   "Israel-Palestinian conflict", "Protests", "Foreign affairs", 
+                   "Christian affairs", "Football", "Culture", "Regional violence", 
+                   "Tourism", "Oil", "Social affairs", "Public economics")
+}
+
+
+# Topic and optimization options
+num.topics <- 20  # Number of topics to model
+num.iterations <- 1000  # Number of training/learning Gibbs sampling iterations
+num.top.words <- 11  # Number of most probable words to print (num.top.words - 1 words, actually)
+optimize.interval <- 20  # Number of iterations between reestimating dirichlet hyperparameters
+optimize.burnin <- 50  # Number of iterations to run before first estimating dirichlet hyperparameters
+
+# Finally, paste the commands together
+# cd <- paste("cd", shQuote(normalizePath(base.directory)))
+cd <- paste("cd", shQuote(normalizePath("../Output")))
+import.command <- paste("../mallet/bin/mallet", "import-dir", "--input", import.dir, 
+                        "--output", output.file, 
+                        "--keep-sequence", 
+                        "--token-regex \"\\w+\"", sep=" ")  # token-regex will consider _ as part of the word
+train.command <- paste("../mallet/bin/mallet", "train-topics", "--input", output.file,
+                       "--num-iterations", num.iterations,
+                       "--num-topics", num.topics,
+                       "--num-top-words", num.top.words, 
+                       "--optimize-interval", optimize.interval, 
+                       "--optimize-burn-in", optimize.burnin, 
+                       "--output-state", output.state, 
+                       "--output-topic-keys", output.topickeys, 
+                       "--output-doc-topics", output.doctopics, 
+                       "--random-seed", mallet.seed, sep=" ")
+
+# And run them all at the same time
+mallet.command <- paste(cd, import.command, train.command, sep=" ; ")
+system(mallet.command)
+
+
+#--------------------
+# Parse the results
+#--------------------
+# Read the files created by MALLET
+topic.keys.result <- read.table(paste("../Output/", output.topickeys, sep=""), header=F, sep="\t")
+doc.topics.result <- read.table(paste("../Output/", output.doctopics, sep=""), header=F, sep="\t")
+
+# MAYBE: Make this *actually* look for (and remove?) the .DS_Store file?
+# if(nrow(doc.topics.result) != 515) {
+#   stop("MALLET accidentally parsed an extra file (like .DS_Store). Delete it manually and rerun this file.")
+# }
+
+# doc.topics.result comes in the following format:
+#
+#   id  file              topic   proportion    topic   proportion    ...
+#   0   ahram_17539.txt   17      0.400385      12      0.3308976     ...
+#   1   ahram_17989.txt   21      0.4516575     13      0.1848781     ...
+#   ...
+#
+# The reshape() and acast() commands below covert that form to the 
+# following matrix, with columns for each topic:
+#
+#   row.name          0      1      2      ...
+#   ahram_17539.txt   0.014  0.007  0.076  ...
+#   ahram_17989.txt   0.005  0.006  0.129  ...
+#   ...
+dat <- doc.topics.result
+doc.topics.long <- reshape(dat, idvar=1:2, varying=list(topics=colnames(dat[,seq(3, ncol(dat)-1, 2)]), 
+                          props=colnames(dat[,seq(4, ncol(dat), 2)])), direction="long")
+doc.topics <- acast(doc.topics.long, V2 ~ V3, value.var="V4")
+row.names(doc.topics) <- basename(row.names(doc.topics))
+rm(doc.topics.long, dat)  # Get rid of these extra data frames
+
+# Normalize topic percentages so that the proportion of articles in each topic sums to 100%
+normalize.topics <- function(x) {
+  x.norm <- t(x)  # Transpose
+  x.norm <- x.norm / rowSums(x.norm)  # Divide by row sums
+  return(data.frame(t(x.norm)))  # Re-transpose
+}
+
+# Example:
+#           Regular                       Normalized        
+# ---------------------------    ---------------------------
+#        topic0 topic1 topic2           topic0 topic1 topic2
+# ahram1    0.4    0.1    0.5    ahram1    0.4  0.143  0.385
+# dne1      0.1    0.3    0.6    dne1      0.1  0.429  0.462
+# egind1    0.5    0.3    0.2    egind1    0.5  0.429  0.154 
+
+# Check on actual data
+# colSums(doc.topics)  # Definitely not 100% in each topic
+# colSums(normalize.topics(doc.topics))  # Definitely 100% in each topic
+
+# Make clean data frames of topic proportions
+topic.docs <- data.frame(doc.topics)  # Regular
+topic.docs.norm <- normalize.topics(doc.topics)  # Normalized
+
+# Add short names to topics
+colnames(topic.keys.result) <- c("key", "dirichlet", "topic.words")
+topic.keys.result$short.names <- short.names
+
+
+#------------------
+# Save everything
+#------------------
+save(topic.keys.result, topic.docs, topic.docs.norm, file=output.rdata)
+
+# Export CSV of topic proportions in documents
+topic.docs.export <- topic.docs.norm
+colnames(topic.docs.export) <- short.names
+write.csv(x=topic.docs.export, file=output.csv, row.names=TRUE)
+
+
+#----------------------------------
+# Create example of normalization
+#----------------------------------
+# Example normalized table
+example.normal <- data.frame(topic1=c(0.5, 0.1, 0.5), topic2=c(0.0, 0.3, 0.3), topic3=c(0.5, 0.6, 0.2))
+rownames(example.normal) <- c("Article 1", "Article 2", "Article 3")
+example.normalized <- normalize.topics(example.normal)
+
+# Add total rows and columns
+example.normal$total <- rowSums(example.normal)
+example.normal <- rbind(example.normal, colSums(example.normal))
+rownames(example.normal)[4] <- "Column totals"
+colnames(example.normal)[4] <- "Row totals"
+example.normal[4,4] <- NA  # Remove column/row sum
+
+example.normalized$total <- rowSums(example.normalized)
+example.normalized <- rbind(example.normalized, colSums(example.normalized))
+rownames(example.normalized)[4] <- "Column totals"
+colnames(example.normalized)[4] <- "Row totals"
+example.normalized[4,4] <- NA  # Remove column/row sum
+
+# Combine tables into mega table
+example.combined <- cbind(example.normal, "", example.normalized)
+colnames(example.combined)[5] <- "→"
+
+# Export to Markdown
+cat(pandoc.table.return(example.combined, split.tables=Inf, 
+                        justify="center", digits=2),
+    file="../Output/table_norm_example.md")
