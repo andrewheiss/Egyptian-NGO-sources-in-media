2689e2c7618f01e07d5982025d0ed44e7726b2d0
Andrew Heiss <andrewheiss@gmail.com>
2014-08-06 14:05:04 -0400

    Create df for organization+article+source

--- R/analyze_topic_model.R
+++ R/analyze_topic_model.R
@@ -1,7 +1,7 @@
 # Title:          analyze_topic_model.R
 # Description:    Do stuff with the topic model
 # Author:         Andrew Heiss
-# Last updated:   2014-08-01
+# Last updated:   2014-08-06
 # R version:      ≥3.0
 
 # Load libraries
@@ -10,19 +10,48 @@ library(reshape2)
 library(ggplot2)
 library(scales)
 library(grid)
+library(XLConnect)
 
-# Load data
-load("../Output/topic_model.RData")
-load("../Output/mentions_data.RData")
+# Load topic and mentions data
+load("../Data/topic_model.RData")
+load("../Data/mentions_data.RData")
+
+# Load NGO data
+# TODO: Don't use Excel, because lame
+org.sources.wb <- loadWorkbook("../Data/source_list.xlsx")
+setMissingValue(org.sources.wb, ".")
+org.sources.full <- readWorksheet(org.sources.wb, sheet="Sheet1")
+
+# Add primary keys
+topic.docs <- topic.docs %>%
+  mutate(article_id = gsub("\\.txt", "", rownames(topic.docs)))
+
+org.sources <- org.sources.full %>%
+  mutate(article_id = paste(publication, id_article, sep="_"))
+
+ngos <- ngos %>%
+  mutate(name.lc = tolower(search.name))
+
+
+# Make mega data frame, listed by organization rows
+org.source.topics <- merge(topic.docs, org.sources, by="article_id") %>%
+  mutate(publication = factor(publication, 
+                              labels=c("Al-Ahram English", "Daily News Egypt", 
+                                       "Egypt Independent"), 
+                              ordered=TRUE)) %>%  # Make a bunch of factors
+  mutate(used_as_source = factor(used_as_source)) %>%
+  mutate(author = factor(author)) %>%
+  mutate(source_type = factor(source_type)) %>%
+  mutate(source_who = factor(source_who)) %>%
+  mutate(name.lc = organization) %>%  # Make primary key
+  left_join(ngos, by="name.lc") %>%  # Bring in nice NGO names
+  mutate(organization = clean.name) %>%  # Rename organization variable
+  select(-c(sentence_number, verbs, sentence_before, sentence, 
+            sentence_after, url, name.lc, search.name, 
+            id_article, clean.name, alternate)) %>%  # Remove extra columns
+  select(c(1, 22:28, 2:21))  # Reorder
 
 # MAYBE: Normalize per organization, not overall (or maybe not normalize?)
-
-# Add publication name
-topic.docs$publication <- factor(regmatches(row.names(topic.docs), regexpr("^[^_]+", row.names(topic.docs))), 
-                                 labels=c("Al-Ahram English", "Daily News Egypt", "Egypt Independent"), ordered=TRUE)
-
-rownames(topic.docs) <- gsub("\\.txt", "", rownames(topic.docs))
-
 
 blah <- melt(all.mentions[,-ncol(all.mentions)], id.vars=c("organization", "publication"), na.rm=TRUE)
 
@@ -33,7 +62,7 @@ blah1 <- blah %>%
 
 
 # Calculate summaries of normalized proportions
-topic.means.wide <- topic.docs.norm %>%
+topic.means.wide <- topic.docs %>%
   group_by(publication) %>%
   summarise_each(funs(mean))  # Or funs(mean, sd) to get both
 
