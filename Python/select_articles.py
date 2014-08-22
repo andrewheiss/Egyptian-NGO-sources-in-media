0d7a42093ac2bd3a7aee6ef48ff4fa37e6483285
Andrew Heiss <andrewheiss@gmail.com>
2014-08-22 14:56:34 -0400

    Save lists of publication and month-year counts to CSV

--- Python/select_articles.py
+++ Python/select_articles.py
@@ -24,6 +24,8 @@ databases = glob.glob('Corpora/*.db')
 
 # Start the dictionary
 mentions = {}
+counts = {}
+month_counts = {}
 
 # Query each database for mentions and save to dictionary
 for db in databases:
@@ -31,17 +33,27 @@ for db in databases:
   conn.row_factory = sqlite3.Row  # Use a dictionary cursor
   c = conn.cursor()
 
-  # Query using the organization names
+  # Get the number of articles in each publication
+  count_sql = 'SELECT COUNT(id_article) AS n FROM articles WHERE article_word_count < 10000 AND article_date BETWEEN \'2011-11-24 00:00:00\' AND \'2013-04-25 23:59:59\''
+  c.execute(count_sql)
+  count_results = c.fetchall()
+
+  # Get the number of articles per month
+  month_sql = 'SELECT strftime(\'%Y-%m\', article_date) as month_year, count(id_article) as n FROM articles WHERE article_date BETWEEN \'2011-11-24 00:00:00\' AND \'2013-04-25 23:59:59\' GROUP BY strftime(\'%Y-%m\', article_date)'
+  c.execute(month_sql)
+  month_results = c.fetchall()
+
+  # Search each corpus for mentions of NGOs
   org_sql = ['article_content_no_punc LIKE "%'+org.lower()+'%"' for org in organizations]
   sql_statement = 'SELECT id_article FROM articles WHERE article_word_count < 10000 AND ('+' OR '.join(org for org in org_sql) + ') AND article_date BETWEEN \'2011-11-24 00:00:00\' AND \'2013-04-25 23:59:59\''
   c.execute(sql_statement)
-
-  # Fetch the results
   results = c.fetchall()
 
   # Save results to dictionary
   publication = os.path.splitext(os.path.basename(db))[0]
   mentions[publication] = [row['id_article'] for row in results]
+  counts[publication] = [row['n'] for row in count_results]
+  month_counts[publication] = [(row['month_year'], row['n']) for row in month_results]
 
 
 #------------------
@@ -58,4 +70,20 @@ with open('Data/ngo_mention_ids.csv', 'w') as f:
   for pub in mentions.keys():
     for id_article in mentions.get(pub):
       row = (pub, id_article)
+      csv_out.writerow(row)
+
+with open('Data/publication_counts.csv', 'w') as f:
+  csv_out = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+  csv_out.writerow(['publication', 'count'])
+  for pub in counts.keys():
+    for count in counts.get(pub):
+      row = (pub, count)
+      csv_out.writerow(row)
+
+with open('Data/month_year_counts.csv', 'w') as f:
+  csv_out = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+  csv_out.writerow(['publication', 'date', 'count'])
+  for pub in month_counts.keys():
+    for month_count in month_counts.get(pub):
+      row = (pub, month_count[0], month_count[1])
       csv_out.writerow(row)
